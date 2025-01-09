45829aec222786c0da8aa3e01e33ef96ce2064c5
josdic1 <joshuadicker@gmail.com>
2025-01-09 12:24:22 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+   name: "Josh Dicker",
+   streetAddress: "240 Highland Road",
+ };
+ 
+ function updateEmployeeWithKeyAndValue(obj, key, value) {
+   return ({
+      ...obj,
+      [key]: value
+   }
+   )
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+   obj[key] = value
+   return obj
+ }
+
+ function deleteFromEmployeeByKey(obj, key) {
+   const newEmployee = { ...obj };
+   delete newEmployee[key];
+   return newEmployee;
+ }
+
+
+ function destructivelyDeleteFromEmployeeByKey(obj, key) {
+   delete obj[key]
+   return obj
+ }
